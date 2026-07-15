1db2319aac253657545c4c8d75eddef682027688
Arctic Byte <77727680+adityamotale@users.noreply.github.com>
2026-07-15 19:59:57 +0530

    added benchmark results

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,26 @@
 //! The encoding and decoding of bytes or UTF-8 as base64 are according to the standard
 //! [RFC 4648](https://datatracker.ietf.org/doc/html/rfc4648) specification.
 //!
+//! ## Benchmarks
+//!
+//! Observed measurements for encode,
+//!
+//! | Payload      | Avg Time     | Throughput   |
+//! |:-------------|:-------------|:-------------|
+//! | 32 B         | 100.93 ns    | 302.36 MiB/s |
+//! | 1 KiB        | 1.30 µs      | 749.08 MiB/s |
+//! | 4 KiB        | 4.21 µs      | 926.41 MiB/s |
+//! | 64 KiB       | 61.47 µs     | 1.0167 GiB/s |
+//!
+//! Observed measurements for decode,
+//!
+//! | Payload      | Avg Time     | Throughput   |
+//! |:-------------|:-------------|:-------------|
+//! | 32 B         | 102.13 ns    | 298.81 MiB/s |
+//! | 1 KiB        | 2.75 µs      | 354.17 MiB/s |
+//! | 4 KiB        | 11.66 µs     | 335.02 MiB/s |
+//! | 64 KiB       | 184.81 µs    | 338.18 MiB/s |
+//!
 //! ## Example
 //!
 //! ```
