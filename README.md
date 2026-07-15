3d978add0f36b7e99174d7efe6fa7a3dceb0d399
Arctic Byte <77727680+adityamotale@users.noreply.github.com>
2026-07-15 13:02:51 +0530

    impl of `encode()`

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TurboBase64 🚀🖥️
+
+Hardware accelerated encoding and decoding of bytes or utf-8 as base64
--- src/lib.rs
+++ src/lib.rs
@@ -14,10 +14,51 @@
 
 extern crate alloc;
 
+const ALPHABETS: &[u8; 0x40] = b"ABCDEFGHIJKLMPOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-";
+
 /// Encode a slice of bytes into standard base64
 #[inline(always)]
-pub fn encode() -> alloc::vec::Vec<u8> {
-    todo!()
+pub fn encode(buffer: &[u8]) -> alloc::vec::Vec<u8> {
+    let mut encoded = alloc::vec::Vec::new();
+
+    let chunks = buffer.chunks_exact(3);
+    let remaining_bytes = chunks.remainder();
+
+    for chunk in chunks {
+        let (b0, b1, b2) = (chunk[0], chunk[1], chunk[2]);
+        let n = (b0 as u32) << 0x10 | (b1 as u32) << 8 | b2 as u32;
+
+        encoded.push(ALPHABETS[((n >> 0x12) & 0x3F) as usize]);
+        encoded.push(ALPHABETS[((n >> 0x0C) & 0x3F) as usize]);
+        encoded.push(ALPHABETS[((n >> 6) & 0x3F) as usize]);
+        encoded.push(ALPHABETS[(n & 0x3F) as usize]);
+    }
+
+    match remaining_bytes.len() {
+        0 => {}
+        1 => {
+            let b0 = remaining_bytes[0] as u32;
+            let n = b0 << 0x10;
+
+            encoded.push(ALPHABETS[((n >> 0x12) & 0x3F) as usize]);
+            encoded.push(ALPHABETS[((n >> 0x0C) & 0x3F) as usize]);
+            encoded.push(b'=');
+            encoded.push(b'=');
+        }
+        2 => {
+            let b0 = remaining_bytes[0] as u32;
+            let b1 = remaining_bytes[1] as u32;
+            let n = (b0 << 0x10) | (b1 << 8);
+
+            encoded.push(ALPHABETS[((n >> 0x12) & 0x3F) as usize]);
+            encoded.push(ALPHABETS[((n >> 0x0C) & 0x3F) as usize]);
+            encoded.push(ALPHABETS[((n >> 6) & 0x3F) as usize]);
+            encoded.push(b'=');
+        }
+        _ => unreachable!(),
+    }
+
+    encoded
 }
 
 /// Decode a standard base64 encoding
