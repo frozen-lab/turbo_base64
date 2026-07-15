d92a81a3641510b6ed3db524b97d61b74d416452
Arctic Byte <77727680+adityamotale@users.noreply.github.com>
2026-07-15 16:07:29 +0530

    added unit test module to test against RFC 4648 standard

--- src/lib.rs
+++ src/lib.rs
@@ -68,6 +68,10 @@ pub fn decode(buffer: &[u8]) -> Result<alloc::vec::Vec<u8>, DecodeError> {
         return Ok(alloc::vec::Vec::new());
     }
 
+    if buffer.len() & 3 != 0 {
+        return Err(DecodeError::InvalidLength);
+    }
+
     let mut padding = 0;
     let mut len = buffer.len();
 
@@ -76,15 +80,23 @@ pub fn decode(buffer: &[u8]) -> Result<alloc::vec::Vec<u8>, DecodeError> {
         padding += 1;
     }
 
-    if len & 3 != 0 {
-        return Err(DecodeError::InvalidLength);
+    if padding > 2 {
+        return Err(DecodeError::InvalidPadding);
     }
 
     let mut decoded = alloc::vec::Vec::with_capacity((buffer.len() / 4) * 3 - padding);
     let mut n = 0u32;
     let mut bits = 0u8;
 
-    for (i, &b) in buffer[..len].iter().enumerate() {
+    for (i, &b) in buffer.iter().enumerate() {
+        if b == b'=' {
+            if i < len {
+                return Err(DecodeError::InvalidPadding);
+            }
+
+            continue;
+        }
+
         let val = match b {
             b'A'..=b'Z' => b - b'A',
             b'a'..=b'z' => b - b'a' + 0x1A,
@@ -117,4 +129,63 @@ pub enum DecodeError {
 
     ///
     InvalidPadding,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use alloc::vec;
+
+    #[test]
+    fn ok_rfc4648_vectors() {
+        // testing against standard RFC 4648 test vectors
+        let tests = [
+            (b"".as_slice(), b"".as_slice()),
+            (b"f".as_slice(), b"Zg==".as_slice()),
+            (b"fo".as_slice(), b"Zm8=".as_slice()),
+            (b"foo".as_slice(), b"Zm9v".as_slice()),
+            (b"foob".as_slice(), b"Zm9vYg== ".as_slice()),
+            (b"fooba".as_slice(), b"Zm9vYmE=".as_slice()),
+            (b"foobar".as_slice(), b"Zm9vYmFy".as_slice()),
+        ];
+
+        for (plain, expected_encoded) in tests {
+            let expected_encoded = expected_encoded.strip_suffix(b" ").unwrap_or(expected_encoded);
+
+            let enc = encode(plain);
+            assert_eq!(enc, expected_encoded);
+
+            let dec = decode(&enc).unwrap();
+            assert_eq!(dec, plain);
+        }
+    }
+
+    #[test]
+    fn test_invalid_length() {
+        assert_eq!(decode(b"Z"), Err(DecodeError::InvalidLength));
+        assert_eq!(decode(b"Zg"), Err(DecodeError::InvalidLength));
+        assert_eq!(decode(b"Zg="), Err(DecodeError::InvalidLength));
+        assert_eq!(decode(b"Zm9vY"), Err(DecodeError::InvalidLength));
+    }
+
+    #[test]
+    fn test_invalid_chars() {
+        assert_eq!(decode(b"Zg ="), Err(DecodeError::InvalidByte(2, b' ')));
+        assert_eq!(decode(b"Zg\n="), Err(DecodeError::InvalidByte(2, b'\n')));
+        assert_eq!(decode(b"Zm9v-mFy"), Err(DecodeError::InvalidByte(4, b'-')));
+    }
+
+    #[test]
+    fn test_invalid_padding_placement() {
+        assert_eq!(decode(b"=m9v"), Err(DecodeError::InvalidPadding));
+        assert_eq!(decode(b"Z=9v"), Err(DecodeError::InvalidPadding));
+    }
+
+    #[test]
+    fn test_binary_roundtrip() {
+        let binary_data: vec::Vec<u8> = (0..=255).collect();
+        let enc = encode(&binary_data);
+        let dec = decode(&enc).unwrap();
+        assert_eq!(dec, binary_data);
+    }
 }
