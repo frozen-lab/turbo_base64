a9ddf9354f49de68ff5b7dfeb304ca466f678284
Arctic Byte <77727680+adityamotale@users.noreply.github.com>
2026-07-16 14:12:06 +0530

    updated benchmark results

--- src/lib.rs
+++ src/lib.rs
@@ -7,21 +7,23 @@
 //!
 //! Observed measurements for encode,
 //!
+//! Observed measurements for encode,
+//!
 //! | Payload      | Avg Time     | Throughput   |
 //! |:-------------|:-------------|:-------------|
-//! | 4 KiB        | 2.82 µs      | 1.35 GiB/s   |
-//! | 64 KiB       | 44.95 µs     | 1.36 GiB/s   |
-//! | 128 KiB      | 99.08 µs     | 1.23 GiB/s   |
-//! | 256 KiB      | 195.10 µs    | 1.25 GiB/s   |
+//! | 4 KiB        | 2.89 µs      | 1.31 GiB/s   |
+//! | 64 KiB       | 45.20 µs     | 1.35 GiB/s   |
+//! | 128 KiB      | 93.78 µs     | 1.05 GiB/s   |
+//! | 256 KiB      | 183.64 µs    | 1.33 GiB/s   |
 //!
 //! Observed measurements for decode,
 //!
 //! | Payload      | Avg Time     | Throughput   |
 //! |:-------------|:-------------|:-------------|
-//! | 32 B         | 102.13 ns    | 298.81 MiB/s |
-//! | 1 KiB        | 2.75 µs      | 354.17 MiB/s |
-//! | 4 KiB        | 11.66 µs     | 335.02 MiB/s |
-//! | 64 KiB       | 184.81 µs    | 338.18 MiB/s |
+//! | 4 KiB        | 4.48 µs      | 872.55 MiB/s |
+//! | 64 KiB       | 70.75 µs     | 883.38 MiB/s |
+//! | 128 KiB      | 142.14 µs    | 879.43 MiB/s |
+//! | 256 KiB      | 292.10 µs    | 855.86 MiB/s |
 //!
 //! ## Example
 //!
